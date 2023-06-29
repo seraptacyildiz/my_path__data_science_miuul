dc7a798dc4a4d70160acf63e6d948dbb1943e5a8
Serap Tacyildiz <126418778+seraptacyildiz@users.noreply.github.com>
2023-06-29 19:28:35 +0300

    Update python_examples_1.py

--- 0_python/python_examples_1.py
+++ 0_python/python_examples_1.py
@@ -1,9 +1,9 @@
 ###############################################
-# Python Alıştırmalar
+# Python Exercises
 ###############################################
 
 ###############################################
-# GÖREV 1: Veri yapılarının tipleriniz inceleyiniz.
+# ASSIGNMENT 1: Check the types of data structures.
 ###############################################
 
 x = 8
@@ -38,37 +38,37 @@ d = {"Name": "Jake",
      "Adress": "Downtown"}
 type(d)
 
-#py 3.7 sürümünden sonra sıralı özelliği kazandı
-# Değiştirilebilir
-# Kapsayıcı
-# Sırasız
-# Key değerleri farklı olacak
+#py Gained the sequential feature after version 3.7
+# Mutable
+# Be nested
+# Unordered
+# Key values are different
 
 
 t = ("Machine Learning", "Data Science")
 type(t)
 
-# Değiştirilemez
-# Kapsayıcı
-# Sıralı
+# Unmutable
+# Be nested
+# Ordered
 
 
 s = {"Python", "Machine Learning", "Data Science","Python"}
 type(s)
 s = {"Python", "ML", "Data Science","Python"}
 
-# Değiştirilebilir
-# Sırasız + Eşsiz
-# Kapsayıcı
+# Mutable
+# Unordered + Unique
+# Be nested
 
 
 
 ###############################################
-# GÖREV 2: Verilen string ifadenin tüm harflerini büyük harfe çeviriniz. Virgül ve nokta yerine space koyunuz, kelime kelime ayırınız.
+# ASSIGNMENT 2: Convert all letters of the given string to uppercase. Put space instead of commas and periods, and separate them word by word.
 ###############################################
 
 ###############################################
-# GÖREV 2: Verilen string ifadenin tüm harflerini büyük harfe çeviriniz. Virgül ve nokta yerine space koyunuz, kelime kelime ayırınız.
+# ASSIGNMENT 2: Convert all letters of the given string to uppercase. Put space instead of commas and periods, and separate them word by word.
 ###############################################
 
 text = "The goal is to turn data into information, and information into insight."
@@ -82,24 +82,24 @@ w
 
 
 ###############################################
-# GÖREV 3: Verilen liste için aşağıdaki görevleri yapınız.
+# ASSIGNMENT 3: Complete the following tasks for the given list.
 ###############################################
 
 lst = ["D","A","T","A","S","C","I","E","N","C","E"]
 
-# Adım 1: Verilen listenin eleman sayısına bakın.
+# Step 1: Check the number of elements of the given list.
 len(lst)
 
-# Adım 2: Sıfırıncı ve onuncu index'teki elemanları çağırın.
+# Step 2: Call the elements at index zero and ten.
 lst[0]
 lst[10]
 
-# Adım 3: Verilen liste üzerinden ["D","A","T","A"] listesi oluşturun. "slicing"
+# Step 3: Create a list ["D","A","T","A"] from the given list. "slicing"
 
 data_list = lst[0:4]
 data_list
 
-# Adım 4: Sekizinci index'teki elemanı silin.
+# Step 4: Delete the element in the eighth index.
 
 del lst[8]
 lst.pop(8)
@@ -108,20 +108,21 @@ lst
 lst.remove("N")
 list = list[1:]
 lst[8] = "N"
-# Adım 5: Yeni bir eleman ekleyin.
+
+# Adım 5: Add a new element.
 
 lst.append(101)
 lst
 
 
-# Adım 6: Sekizinci index'e  "N" elemanını tekrar ekleyin.
+# Step 6: Add the "N" element back to the eighth index.
 
 lst.insert(8, "N")
 lst
 
 
 ###############################################
-# GÖREV 4: Verilen sözlük yapısına aşağıdaki adımları uygulayınız.
+# ASSIGNMENT 4: Apply the following steps to the given dictionary structure.
 ###############################################
 
 dict = {'Christian': ["America",18],
@@ -130,15 +131,16 @@ dict = {'Christian': ["America",18],
         'Dante':["Italy",25]}
 
 
-# Adım 1: Key değerlerine erişiniz.
+# Step 1: Get the Key values.
 
 dict.keys()
 
-# Adım 2: Value'lara erişiniz.
+# Step 2: Get the Values.
 
 dict.values()
 
-# Adım 3: Daisy key'ine ait 12 değerini 13 olarak güncelleyiniz.
+# Step 3: Update the value 12 of the Daisy key to 13.
+
 dict.update({"Daisy": ["England",13]})
 dict
 
@@ -149,12 +151,12 @@ dict["Daisy"][0]="UK"
 dict
 
 
-# Adım 4: Key değeri Ahmet value değeri [Turkey,24] olan yeni bir değer ekleyiniz.
+# Step 4: Add a new value whose key value is Ahmet value [Turkey,24].
 
 dict.update({"Ahmet": ["Turkey", 24]})
 dict
 
-# Adım 5: Antonio'yu dictionary'den siliniz.
+# Step 5: Delete Antonio from dict.
 
 dict.pop("Antonio")
 dict
@@ -170,26 +172,26 @@ dict["ali"]=['America', 18]
 
 dict["aaa"] = dict.pop("ali")
 ###############################################
-# GÖREV 5: Arguman olarak bir liste alan, listenin içerisindeki tek ve çift sayıları ayrı listelere atıyan ve bu listeleri return eden fonskiyon yazınız.
+# ASSIGNMENT 5: Write a function that takes a list as an argument, assigns the odd and even numbers in the list to separate lists, and returns these lists.
 ###############################################
 
 l = [2,13,18,93,22]
 
 def func(list):
 
-    çift_list = []
-    tek_list = []
+    even_list = []
+    odd_list = []
 
     for i in list:
         if i % 2 == 0:
-            çift_list.append(i)
+            even_list.append(i)
         else:
-            tek_list.append(i)
+            odd_list.append(i)
 
-    return çift_list, tek_list
+    return even_list, odd_list
 
 
-çift,tek = func(l)
+even, odd = func(l)
 
 def func(l):
     list = [[], []]
@@ -213,89 +215,90 @@ def function(x):
     return even_list, odd_list
 function(l)
 
-def ayir(liste):
-    cift = []
-    tek = []
-    [cift.append(i) if i%2==0 else tek.append(i) for i in liste]
-    return(tek, cift)
+def separate(liste):
+    even = []
+    odd = []
+    [even.append(i) if i%2==0 else odd.append(i) for i in liste]
+    return(odd, even)
 
-ayir(l)
+separate(l)
 
 
 ###############################################
-# GÖREV 6: Aşağıda verilen listede mühendislik ve tıp fakülterinde dereceye giren öğrencilerin isimleri bulunmaktadır.
-# Sırasıyla ilk üç öğrenci mühendislik fakültesinin başarı sırasını temsil ederken son üç öğrenci de tıp fakültesi öğrenci sırasına aittir.
-# Enumarate kullanarak öğrenci derecelerini fakülte özelinde yazdırınız.
+# ASSIGNMENT 6: In the list given below are the names of the students who received degrees in engineering and medicine faculties.
+# While the first three students represent the success order of the engineering faculty, the last three students belong to the medical faculty student rank, respectively.
+# Print the student's degrees specific to the faculty using Enumarate.
 ###############################################
 
-ogrenciler = ["Ali","Veli","Ayşe","Talat","Zeynep","Ece"]
+students = ["Ali","Veli","Ayşe","Talat","Zeynep","Ece"]
 
 
-for i,x in enumerate(ogrenciler):
+for i,x in enumerate(students):
     if i<3:
         i += 1
-        print("Mühendislik Fakültesi",i,". öğrenci: ",x)
+        print("Engineering Faculty",i,". student: ",x)
     else:
         i -= 2
-        print("Tıp Fakültesi",i,". öğrenci: ",x)
+        print("Medicine Faculty",i,". student: ",x)
 
-for i,x in enumerate(ogrenciler):
+for i,x in enumerate(student):
     i -= 2
     print(i,x)
 
 
-for index, ogrenci in enumerate(ogrenciler, 1):
+for index, student in enumerate(students, 1):
     if index < 4:
-        print("Mühendislik Fakültesi", index, ". öğrenci:", ogrenci)
+        print("Engineering Faculty", index, ". student:", student)
     else:
-        print("Tıp Fakültesi", index-3, ". öğrenci:", ogrenci)
+        print("Medicine Faculty", index-3, ". student:", student)
 
-a = ogrenciler[0:3]
-b = ogrenciler[3:]
+a = students[0:3]
+b = students[3:]
 
-for index, ogrenci in enumerate(a, 1):
-    print(f"Muhendislik Fakultesi {index}. ogrenci: {ogrenci}")
+for index, student in enumerate(a, 1):
+    print(f"Engineering Faculty {index}. student: {ogrenci}")
 for index, ogrenci in enumerate(b, 1):
-    print(f"Tıp Fakultesi {index}. ogrenci: {ogrenci}")
+    print(f"Medicine Faculty {index}. student: {student}")
 
 for i, ogrenci in enumerate(ogrenciler, 1):
     if i <= 3:
-        print("Mühendislik Fakültesi", i, ". öğrenci:", ogrenci)
+        print("Engineering Faculty", i, ". student:", student)
     else:
-        print("Tıp Fakültesi", i-3, ". öğrenci:", ogrenci)
+        print("Medicine Faculty", i-3, ". student:", student)
 
-Mühendislik_Fakültesi=[]
-Tıp_Fakültesi=[]
-fakulte=[[],[]]
-for i,x in enumerate(ogrenciler,1):
+Engineering_Faculty=[]
+Medicine_Faculty=[]
+faculty=[[],[]]
+for i,x in enumerate(students,1):
     if i<4:
         fakulte[0].append(x)
-        print(f"Mühendislik Fakültesi öğrencisi {i}: {x}")
+        print(f"Engineering Faculty students are {i}: {x}")
     else:
         fakulte[1].append(x)
-        print(f"Tıp Fakültesi öğrencisi {i-3}: {x}")
+        print(f"Medicine Faculty students are {i-3}: {x}")
 
-for i, ogrenci in enumerate(ogrenciler):
+for i, student in enumerate(students):
     if i < 3:
-        print(f"Mühendislik Fakültesi {i+1}. öğrencisi: {ogrenci}")
+        print(f"Engineering Faculty {i+1}. student: {student}")
     else:
-        print(f"Tıp Fakültesi {i-2}. öğrencisi: {ogrenci}")
+        print(f"Medicine Faculty {i-2}. student: {student}")
 
 ###############################################
-# GÖREV 7: Aşağıda 3 adet liste verilmiştir. Listelerde sırası ile bir dersin kodu, kredisi ve kontenjan bilgileri yer almaktadır. Zip kullanarak ders bilgilerini bastırınız.
+# ASSIGNMENT 7: Three lists are given below. In the lists, there is a course code, credit and quota information, respectively. Print course information using zip.
 ###############################################
 
-ders_kodu = ["CMP1005","PSY1001","HUK1005","SEN2204"]
-kredi = [3,4,2,4]
-kontenjan = [30,75,150,25]
+course_code = ["CMP1005","PSY1001","HUK1005","SEN2204"]
+credit = [3,4,2,4]
+quota = [30,75,150,25]
 
-for ders_kodu, kredi, kontenjan in zip(ders_kodu, kredi, kontenjan):
-  print(f"Kredisi {kredi} olan {ders_kodu} kodlu dersin kontenjanı {kontenjan} kişidir.")
+for course_code, credit, quota in zip(course_code, credit, quota):
+  print(f"The quota of the {course_code} coded course with {credit} credits is {quota} people.")
+
 
 
 ###############################################
-# GÖREV 8: Aşağıda 2 adet set verilmiştir.
-# Sizden istenilen eğer 1. küme 2. kümeyi kapsiyor ise ortak elemanlarını eğer kapsamıyor ise 2. kümenin 1. kümeden farkını yazdıracak fonksiyonu tanımlamanız beklenmektedir.
+# ASSIGNMENT 8: Below are 2 sets.
+# You are asked to define the function that will print the difference of the 2nd set from the 1st set, if the 1st set includes the 2nd set, if it does not cover their common elements.
 ###############################################
 
 kume1 = set(["data", "python"])
